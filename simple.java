3ef24d3deb4a3783df23563ffeecb35b4419d464
Pooja Kale <Poojakale0911@gmail.com>
2022-03-12 14:38:59 +0530

    Add Simple.Java File.

---
+++ simple.java
@@ -0,0 +1,10 @@
+//   Write a Java Program to print “Hello and Welcome to Java “ message.
+
+public class simple
+
+{
+      public static void main(String args[])
+      {
+           System.out.println("Hello and Welcome to Java");
+      }
+}
