8337755d4fa26a9e8a99720f47a34ef8225346bd
Pooja Kale <Poojakale0911@gmail.com>
2022-03-12 14:44:01 +0530

    Second : Add Simple.Java File.

--- simple.java
+++ simple.java
@@ -6,5 +6,9 @@ public class simple
       public static void main(String args[])
       {
            System.out.println("Hello and Welcome to Java");
+           int a=100, b=120;
+           int sum =0;
+           sum = a + b;
+           System.out.println("Addition Is : " sum);
       }
 }
